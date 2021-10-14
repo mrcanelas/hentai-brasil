d34812350b2ceefa93712eb8164420dfc3efe7c3
mrcanelas <sialsan@hotmail.com>
2021-10-14 09:49:09 -0300

    add schedule for scraper

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
 
+const schedule = require('node-schedule');
+const { getMapping } = require("./lib/mapper");
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./addon")
-const { PORT } = require("./lib/connect")
-serveHTTP(addonInterface, { port: PORT })
+const { PORT, connect } = require("./lib/connect")
 
+serveHTTP(addonInterface, { port: PORT })
+.then(res => {
+    connect().then((uri) => {
+        console.log(`MONGO URI: ${uri}`)
+        schedule.scheduleJob('* * 3 * * ', function(){
+            console.log('Scraper started');
+            getMapping()
+          });
+    }).catch(console.error)
+})
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
 // for more information on deploying, see: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/deploying/README.md
